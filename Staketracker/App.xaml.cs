a3e57042b9886cd42bd4b22221328573e3ffee78
Andinet A <andyasne@gmail.com>
2021-02-25 12:51:37 +0300

    Added APIServices File

--- Staketracker/App.xaml.cs
+++ Staketracker/App.xaml.cs
@@ -3,17 +3,27 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Staketracker.Services;
 using Staketracker.Views;
+using Xamarin.Essentials;
 
 namespace Staketracker
 {
     public partial class App : Application
     {
+        //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
+        //To debug on Android emulators run the web backend against .NET Core not IIS
+        //If using other emulators besides stock Google images you may need to adjust the IP address
+        public static string StaketrackerBackendUrl =
+            DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : "http://localhost:5000";
+        public static bool UseMockDataStore = true;
 
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
+            if (UseMockDataStore)
+                DependencyService.Register<MockDataStore>();
+            else
+                DependencyService.Register<APIServices>();
             MainPage = new MainPage();
         }
 
