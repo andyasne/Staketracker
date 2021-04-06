2fdd2998f2a3ede932833a29bad4333a47a1c69c
Andinet Asamnew <andyasne@live.com>
2021-04-06 15:55:27 +0300

    Added Splash Screen Activity

--- src/Staketracker.Droid/Resources/Resource.Designer.cs
+++ src/Staketracker.Droid/Resources/Resource.Designer.cs
@@ -17654,13 +17654,16 @@ namespace Staketracker.Droid
 			public const int preference_list_divider_material = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int splash_screen = 2131165314;
+			public const int splash_logo = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int tooltip_frame_dark = 2131165315;
+			public const int splash_screen = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int tooltip_frame_light = 2131165316;
+			public const int tooltip_frame_dark = 2131165316;
+			
+			// aapt resource value: 0x7F070085
+			public const int tooltip_frame_light = 2131165317;
 			
 			static Drawable()
 			{
