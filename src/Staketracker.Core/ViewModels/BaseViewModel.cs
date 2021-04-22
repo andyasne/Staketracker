b8113ed04f1dec933ed861f28194d22322de7c07
Andinet Asamnew <andyasne@live.com>
2021-04-22 13:59:29 +0300

    Implemented BaseViewModel Api Manager

--- src/Staketracker.Core/ViewModels/BaseViewModel.cs
+++ src/Staketracker.Core/ViewModels/BaseViewModel.cs
@@ -1,8 +1,59 @@
 using MvvmCross.ViewModels;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
+using Staketracker.Core.Services;
+
 
 namespace Staketracker.Core.ViewModels
 {
-    public abstract class BaseViewModel : MvxViewModel
+    public abstract class BaseViewModel : MvxViewModel, INotifyPropertyChanged
     {
+
+        public IUserDialogs PageDialog = UserDialogs.Instance;
+        public IApiManager ApiManager;
+        IApiService<IMakeUpApi> makeUpApi = new ApiService<IMakeUpApi>(Config.ApiUrl);
+        IApiService<IRedditApi> redditApi = new ApiService<IRedditApi>(Config.RedditApiUrl);
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsBusy { get; set; }
+        public BaseViewModel()
+        {
+            ApiManager = new ApiManager(makeUpApi, redditApi);
+
+        }
+
+        public async Task RunSafe(Task task, bool ShowLoading = true, string loadinMessage = null)
+        {
+            try
+            {
+                if (IsBusy)
+                    return;
+
+                IsBusy = true;
+
+                if (ShowLoading)
+                    UserDialogs.Instance.ShowLoading(loadinMessage ?? "Loading");
+
+                await task;
+            }
+            catch (Exception e)
+            {
+                IsBusy = false;
+                UserDialogs.Instance.HideLoading();
+                Debug.WriteLine(e.ToString());
+
+                await PageDialog.AlertAsync("Check your internet connection", "Error", "Ok");
+            }
+            finally
+            {
+                IsBusy = false;
+                if (ShowLoading)
+                    UserDialogs.Instance.HideLoading();
+            }
+        }
+
     }
 }
