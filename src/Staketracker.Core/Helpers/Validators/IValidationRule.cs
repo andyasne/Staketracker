ed0650480f369ec141b97b582eb0c650d510a6a5
Andinet Asamnew <andyasne@live.com>
2021-05-04 04:47:17 +0300

    Added  Files for Page Validations

---
+++ src/Staketracker.Core/Helpers/Validators/IValidationRule.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Staketracker.Core.Validators
+{
+    public interface IValidationRule<T>
+    {
+        string ValidationMessage { get; set; }
+        bool Check(T value);
+    }
+}
