df252472b49c0f8da5d95c935bea5a33fe21c759
Andinet Asamnew <andyasne@live.com>
2021-05-04 04:47:17 +0300

    Added  Files for Page Validations

---
+++ src/Staketracker.Core/Helpers/Validators/IValidatable.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Staketracker.Core.Validators
+{
+    public interface IValidatable<T> : INotifyPropertyChanged
+    {
+        List<IValidationRule<T>> Validations { get; }
+
+        List<string> Errors { get; set; }
+
+        bool Validate();
+
+        bool IsValid { get; set; }
+    }
+}
