0bf17c41c4c8231df6b58d9156abce9bd02e62c6
Andinet Asamnew <andyasne@live.com>
2021-05-11 00:22:14 +0300

    Added Events List with Test Data

--- src/Staketracker.Core/ViewModels/EventsList/EventsListViewModel.cs
+++ src/Staketracker.Core/ViewModels/EventsList/EventsListViewModel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using MvvmCross.Navigation;
@@ -6,17 +9,84 @@ using Xamarin.Forms;
 
 namespace Staketracker.Core.ViewModels.EventsList
 {
+
+    public class Customer
+    {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Uri ImageURL { get; set; }
+
+    }
+
     public class EventsListViewModel : BaseViewModel
 
     {
+        private Customer selectedCustomer, selectedCustomerDetail;
+        private ObservableCollection<Customer> customers;
+        private string listDescription;
+
         private readonly IMvxNavigationService _navigationService;
 
-      
         public EventsListViewModel(IMvxNavigationService navigationService)
         {
+
+            this.listDescription = "All Customers";
+
+            //this.customers = new ObservableCollection<Customer> { new Customer("Tom"), new Customer("Anna"), new Customer("Peter"), new Customer("Teodor"), new Customer("Lorenzo"), new Customer("Andrea"), new Customer("Martin") };
+
             _navigationService = navigationService;
 
-         }
-         
+
+
+        }
+
+        private async Task FetchData()
+        {
+            Customer cust = new Customer();
+            cust.Name = "TEST";
+            Customer cust1 = new Customer();
+            cust1.Name = "TEST-1";
+            Customer cust2 = new Customer();
+            cust2.Name = "TEST-2";
+            this.customers = new ObservableCollection<Customer>();
+            customers.Add(cust);
+            customers.Add(cust1);
+            customers.Add(cust2);
+        }
+        public async override void Prepare()
+        {
+            base.Prepare();
+            await this.FetchData();
+        }
+        public ObservableCollection<Customer> Customers
+        {
+            get => customers;
+            private set => SetField(ref customers, value);
+        }
+
+        public Customer SelectedCustomer
+        {
+            get => selectedCustomer;
+            set
+            {
+                if (SetProperty(ref selectedCustomer, value) && value != null)
+                {
+                    SetField(ref selectedCustomer, value);
+
+                }
+            }
+        }
+        public async Task Refresh()
+        {
+
+        }
+        public string ListDescription
+        {
+            get => listDescription;
+            private set => SetProperty(ref listDescription, value);
+        }
+
+
     }
 }
--- src/Staketracker.UI/Pages/EventsList/EventsListPage.xaml.cs
+++ src/Staketracker.UI/Pages/EventsList/EventsListPage.xaml.cs
@@ -1,17 +1,60 @@
 using MvvmCross.Forms.Presenters.Attributes;
 using MvvmCross.Forms.Views;
 using Staketracker.Core.ViewModels.EventsList;
+using Telerik.XamarinForms.DataControls;
 using Xamarin.Forms.Xaml;
+using MvvmCross.Forms.Presenters.Attributes;
+using MvvmCross.Forms.Views;
+using MvvmCross.Presenters;
+using MvvmCross.Presenters.Attributes;
+using MvvmCross.ViewModels;
+using Telerik.XamarinForms.DataControls;
+using Xamarin.Forms;
 
 namespace Staketracker.UI.Pages.EventsList
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    [MvxMasterDetailPagePresentation(Position = MasterDetailPosition.Detail, NoHistory = true, Title = "Staketracker Dashboard")]
+    [MvxMasterDetailPagePresentation(Position = MasterDetailPosition.Detail, NoHistory = true, Title = "SustaiNet Core")]
     public partial class EventsListPage : MvxContentPage<EventsListViewModel>
     {
         public EventsListPage()
         {
             InitializeComponent();
+
+            if (Device.Idiom != TargetIdiom.Phone)
+            {
+                var service = MvvmCross.Mvx.IoCProvider.Resolve<IMvxViewModelLoader>();
+                var vm = service.LoadViewModel(new MvxViewModelInstanceRequest(typeof(Staketracker.Core.ViewModels.EventsList.EventsListViewModel)), null);
+                ViewModel = vm as EventsListViewModel;
+
+                NavigationPage.SetHasNavigationBar(this, false);
+                Grid.SetRowSpan(this.busyIndicator, 2);
+                BackgroundColor = Color.FromHex("#f1f3f7");
+            }
+            else
+            {
+                this.IconImageSource = new FileImageSource() { File = "search" };
+                this.LayoutRoot.Children.Remove(this.searchBar);
+                this.LayoutRoot.RowDefinitions.Clear();
+                this.ContentRoot.ClearValue(Grid.RowProperty);
+
+                var searchToolbarItem = new ToolbarItem();
+                searchToolbarItem.Text = "Search";
+                searchToolbarItem.IconImageSource = new FileImageSource() { File = "search" };
+                searchToolbarItem.SetBinding(ToolbarItem.CommandProperty, new Binding("SearchCommand"));
+
+
+
+                this.ToolbarItems.Add(searchToolbarItem);
+
+            }
         }
+
+        private async void Handle_RefreshRequested(object sender, Telerik.XamarinForms.DataControls.ListView.PullToRefreshRequestedEventArgs e)
+        {
+            await ViewModel.Refresh();
+            (sender as RadListView).IsPullToRefreshActive = false;
+        }
+
     }
 }
