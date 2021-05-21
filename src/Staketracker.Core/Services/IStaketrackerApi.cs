719a6449bd24090a3aa695dceeffe512c4f67abf
Andinet A <andyasne@gmail.com>
2021-05-21 23:56:09 +0300

    Changed API Address

--- src/Staketracker.Core/Services/IStaketrackerApi.cs
+++ src/Staketracker.Core/Services/IStaketrackerApi.cs
@@ -10,18 +10,18 @@ namespace Staketracker.Core
     [Headers("Content-Type: application/json")]
     public interface IStaketrackerApi
     {
-        [Post("/uat/mobilev1.asmx/authenticateUser")]
+        [Post("/demo/mobilev1.asmx/authenticateUser")]
         Task<HttpResponseMessage> AuthenticateUser([Body] LoginAPIBody loginApiBody, CancellationToken cancellationToken);
 
 
-        [Post("/uat/mobilev1.asmx/Is2FEnabled")]
+        [Post("/demo/mobilev1.asmx/Is2FEnabled")]
         Task<HttpResponseMessage> Is2FEnabled([Body] LoginAPIBody loginApiBody, CancellationToken cancellationToken);
 
 
-        [Post("/uat/mobilev1.asmx/getUsrEmail")]
+        [Post("/demo/mobilev1.asmx/getUsrEmail")]
         Task<HttpResponseMessage> GetUsrEmail([Body] LoginAPIBody loginApiBody, CancellationToken cancellationToken);
 
-        [Post("/uat/mobilev1.asmx/getEvents")]
+        [Post("/demo/mobilev1.asmx/getEvents")]
         Task<HttpResponseMessage> GetEvents([Body] APIRequestBody aPIRequestBody, [Header("sessionId")] string sessionId, CancellationToken cancellationToken);
 
     }
