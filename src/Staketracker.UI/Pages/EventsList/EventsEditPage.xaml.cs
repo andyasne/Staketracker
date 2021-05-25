6c0b4e5fe124144d67646241db863f39e26d07f0
Andinet A <andyasne@gmail.com>
2021-05-25 17:42:16 +0300

    Added Header Menu Items

--- src/Staketracker.UI/Pages/EventsList/EventsEditPage.xaml.cs
+++ src/Staketracker.UI/Pages/EventsList/EventsEditPage.xaml.cs
@@ -14,16 +14,60 @@ using Staketracker.Core;
 
 namespace Staketracker.UI.Pages.EventsList
 {
-    public partial class EventsEditPage : MvxContentPage<SEventDetailViewModel>
+    public partial class EventsEditPage : MvxContentPage<SEventDetailViewModel>, IPopupHost
     {
+        private ToolbarItem saveToolbarItem;
+        private ToolbarItem deleteToolbarItem;
+
         public EventsEditPage()
         {
             InitializeComponent();
 
+            saveToolbarItem = new ToolbarItem();
+            saveToolbarItem.Text = "Save";
+            //     checkToolbarItem.IconImageSource = new FileImageSource() { File = "check" };
+            //   checkToolbarItem.SetBinding(ToolbarItem.CommandProperty, new Binding("CommitCommand"));
+            Telerik.XamarinForms.Primitives.RadPopup.SetPopup(this, null);
+
+
+            deleteToolbarItem = new ToolbarItem();
+            deleteToolbarItem.Text = "Delete";
+            //     checkToolbarItem.IconImageSource = new FileImageSource() { File = "check" };
+            //   checkToolbarItem.SetBinding(ToolbarItem.CommandProperty, new Binding("CommitCommand"));
+
+        }
+        protected override void OnAppearing()
+        {
+            this.ToolbarItems.Add(saveToolbarItem);
+            this.ToolbarItems.Add(deleteToolbarItem);
+
 
         }
 
- 
+        public void ClosePopup()
+        {
+            this.popup.IsOpen = false;
+        }
 
+        public void OpenPopup()
+        {
+            this.popup.IsOpen = true;
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            this.ClosePopup();
+        }
+        public MvxBasePresentationAttribute PresentationAttribute(MvxViewModelRequest request)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                return new MvxContentPagePresentationAttribute() { WrapInNavigationPage = true };
+            }
+            else
+            {
+                return new MvxCustomMasterDetailPagePresentationAttribute(MasterDetailPosition.Detail) { NoHistory = true, MasterHostViewType = typeof(SEventsListPage) };
+            }
+        }
     }
 }
