6b8b9f9b6b22af79175b4a2ac9108e1cfda270b3
Andinet A <andyasne@gmail.com>
2021-07-10 17:18:22 +0300

    Modified IOS Project

--- src/Staketracker.iOS/AppDelegate.cs
+++ src/Staketracker.iOS/AppDelegate.cs
@@ -1,10 +1,14 @@
-﻿using Foundation;
+using Foundation;
 using MvvmCross.Forms.Platforms.Ios.Core;
 
 namespace Staketracker.iOS
 {
     [Register(nameof(AppDelegate))]
     public partial class AppDelegate : MvxFormsApplicationDelegate<Setup, Core.App, UI.App>
+    {
+    }
+
+    public class Setup : MvxFormsIosSetup<Core.App, UI.App>
     {
     }
 }
