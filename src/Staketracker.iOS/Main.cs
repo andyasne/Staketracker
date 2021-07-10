b8c7a61f45deced9f4c6a0be5cb80bcb37fed92f
Andinet A <andyasne@gmail.com>
2021-07-10 17:18:22 +0300

    Modified IOS Project

--- src/Staketracker.iOS/Main.cs
+++ src/Staketracker.iOS/Main.cs
@@ -1,12 +1,20 @@
-﻿using UIKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
 
 namespace Staketracker.iOS
 {
-    public static class Application
+    public class Application
     {
-        private static void Main(string[] args)
+        // This is the main entry point of the application.
+        static void Main(string[] args)
         {
-            UIApplication.Main(args, null, nameof(AppDelegate));
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
         }
     }
 }
