a586128b6e9bbc53442987cc463203ab74794ffd
Andinet A <andyasne@gmail.com>
2021-07-13 22:18:19 +0300

    Android UI Fix

---
+++ src/Staketracker.Core/Themes/DashboardStyles.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using Xamarin.Forms;
+
+namespace Staketracker.UI.Themes
+{
+    public partial class DashboardStyles : ResourceDictionary
+    {
+        public DashboardStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Staketracker.Core/Themes/LoginStyles.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using Xamarin.Forms;
+
+namespace Staketracker.UI.Themes
+{
+    public partial class LoginStyles : ResourceDictionary
+    {
+        public LoginStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
