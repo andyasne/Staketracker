c3cf6f98d07aaa326aa9d81c8fe12b51472d4f90
Andinet Asamnew <andyasne@gmail.com>
2021-10-12 23:56:16 +0300

    removed password control

--- src/Staketracker.UI/Pages/LoginPage.xaml.cs
+++ src/Staketracker.UI/Pages/LoginPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using MvvmCross.Forms.Presenters.Attributes;
 using MvvmCross.Forms.Views;
 using MvvmCross.Presenters;
@@ -42,6 +43,37 @@ namespace Staketracker.UI.Pages
             return new MvxMasterDetailPagePresentationAttribute() { WrapInNavigationPage = true, NoHistory = true };
 
         }
+
+    }
+
+    public class ShowPasswordTriggerAction : TriggerAction<ImageButton>, INotifyPropertyChanged
+    {
+        public string ShowIcon { get; set; }
+        public string HideIcon { get; set; }
+
+        bool _hidePassword = true;
+
+        public bool HidePassword
+        {
+            set
+            {
+                if (_hidePassword != value)
+                {
+                    _hidePassword = value;
+
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HidePassword)));
+                }
+            }
+            get => _hidePassword;
+        }
+
+        protected override void Invoke(ImageButton sender)
+        {
+            sender.Source = HidePassword ? ShowIcon : HideIcon;
+            HidePassword = !HidePassword;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
     }
 }
