4e93ec7fbd6212b80c498dc6088d3572b67b1b14
Andinet Asamnew <andyasne@gmail.com>
2021-11-17 15:50:34 +0300

    Fix for Communication Loader

--- src/Staketracker.Core/ViewModels/Communication/CommunicationDetailViewModel.cs
+++ src/Staketracker.Core/ViewModels/Communication/CommunicationDetailViewModel.cs
@@ -1,26 +1,16 @@
-using MvvmCross.Commands;
-using MvvmCross.Navigation;
-using Newtonsoft.Json;
-using Staketracker.Core.Helpers;
-using Staketracker.Core.Models;
-using Staketracker.Core.Models.ApiRequestBody;
-using Staketracker.Core.Models.FieldsValue;
-using Staketracker.Core.Models.FormAndDropDownField;
-using Staketracker.Core.Validators;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Staketracker.Core.ViewModels.CommunicationList;
-using Xamarin.Forms;
-using PresentationMode = Staketracker.Core.Models.PresentationMode;
-using Staketracker.Core.Models.Stakeholders;
-using Staketracker.Core.Models.Communication;
-using Staketracker.Core.Models.EventsFormValue;
-
 namespace Staketracker.Core.ViewModels.Communication
 {
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using MvvmCross.Commands;
+    using MvvmCross.Navigation;
+    using Staketracker.Core.Helpers;
+    using Staketracker.Core.Models;
+    using Staketracker.Core.Models.Communication;
+    using Staketracker.Core.Models.EventsFormValue;
+    using Staketracker.Core.ViewModels.CommunicationList;
+    using PresentationMode = Staketracker.Core.Models.PresentationMode;
+
     public class CommunicationDetailViewModel : BaseViewModel<PresentationContext<AuthReply>>
     {
         public CommunicationDetailViewModel(IMvxNavigationService navigationService)
@@ -30,6 +20,7 @@ namespace Staketracker.Core.ViewModels.Communication
             SaveCommand = new MvxAsyncCommand(OnSave);
             BeginEditCommand = new MvxAsyncCommand(OnBeginEdit);
         }
+
         public override void Prepare(PresentationContext<AuthReply> parameter)
         {
             authReply = parameter.Model;
@@ -37,22 +28,30 @@ namespace Staketracker.Core.ViewModels.Communication
             primaryKey = parameter.PrimaryKey;
             name = parameter.Name;
         }
+
         public override async void ViewAppearing()
         {
             if (mode == PresentationMode.Edit || mode == PresentationMode.Read)
             {
-                var apiReqExtra = new APIRequestExtraBody(authReply, "PrimaryKey", primaryKey.ToString());
-                HttpResponseMessage communications = await ApiManager.GetCommunicationDetails(new jsonTextObj(apiReqExtra.GetJsonText()), authReply.d.sessionId);
+                CommunicationDetailReq body = new CommunicationDetailReq()
+                {
+                    projectId = authReply.d.projectId,
+                    userId = authReply.d.userId,
+                    ID = primaryKey
+                };
+
+                HttpResponseMessage communications = await ApiManager.GetCommunicationDetails(new jsonTextObj(body), authReply.d.sessionId);
                 PopulateControlsWithData(authReply, primaryKey, communications);
             }
-
         }
+
         public override async Task Initialize()
         {
             await base.Initialize();
             RunSafe(GetFormUIControls(authReply, FormType.Communications), true, "Building Form Controls");
             UpdateTitle();
         }
+
         private async Task OnDelete()
         {
             var result = await ShowDeleteConfirmation();
@@ -63,18 +62,21 @@ namespace Staketracker.Core.ViewModels.Communication
                 NavigateToList();
             }
         }
+
         private async Task NavigateToList()
         {
             await navigationService.ChangePresentation(
             new MvvmCross.Presenters.Hints.MvxPopPresentationHint(typeof(CommunicationListViewModel)));
             return;
         }
+
         internal async Task AddCommunication()
         {
             jsonTextObj jsonTextObj = new jsonTextObj(pageFormValue);
             HttpResponseMessage communications = await ApiManager.AddCommunication(jsonTextObj, authReply.d.sessionId);
             await Add(communications);
         }
+
         private async Task OnSave()
         {
             if (isFormValid())
@@ -84,8 +86,6 @@ namespace Staketracker.Core.ViewModels.Communication
                 changeView();
 
             }
-
-
         }
     }
 }
