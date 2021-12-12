517303a2eb398d3cb1f84e1cdb61276403c97175
Andinet Asamnew <andyasne@gmail.com>
2021-12-12 17:11:58 +0300

    Fix for on Load Error

--- src/Staketracker.Core/Models/FieldsValue.cs
+++ src/Staketracker.Core/Models/FieldsValue.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
+using Staketracker.Core.Models.FormAndDropDownField;
 
 namespace Staketracker.Core.Models.FieldsValue
 {
-
 
     public class Field
     {
@@ -13,11 +13,11 @@ namespace Staketracker.Core.Models.FieldsValue
         public string ValidationType { get; set; }
         public bool MandatoryField { get; set; }
         public string ColumnName { get; set; }
-        public List<Staketracker.Core.Models.FormAndDropDownField.DropdownValue> DropdownValues { get; set; }
+        public IList<DropdownValue> DropdownValues { get; set; }
         public string GroupID { get; set; }
         public string DraftSection { get; set; }
         public object Value { get; set; }
-        public int? SelectedKey { get; set; }
+        public object SelectedKey { get; set; }
         public bool IsHidden { get; set; }
         public bool IsGroupRequired { get; set; }
     }
